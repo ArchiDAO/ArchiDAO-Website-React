ee827d0f864bee5e3a22c1b56c6b07baba6029bc
Barbara Villanova <villanovaarch@gmail.com>
2022-10-24 23:03:20 -0300

    cards update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,10 @@ import React from "react";
 import '../styles/Cards.css';
 import projects from './Projects.json';
 
+
 const Cards = () => {
+   
+ 
     console.log(projects);
     const cards = projects.Projects.map((project) => {
         return (    
@@ -12,7 +15,7 @@ const Cards = () => {
                             <img className="category__01" src="./logo.jpg" alt="Project" />
                         </picture>
                         <div className="card-content">
-                            <p className="category category__01">{project.category}</p>
+                            <p className="category category__01 tag">{project.category}</p>
                             <h2>{project.name}</h2>
                             <p className="description">{project.description}</p>
                         </div>
